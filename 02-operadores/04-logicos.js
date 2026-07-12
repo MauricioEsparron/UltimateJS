a95c4a0033bc3d4e93dde621fa27d2ba58fe0598
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-12 13:13:31 -0500

    feat: experimentar con operadores lógicos en JavaScript
    - Se declaran variables booleanas para control de acceso ficticio.
    - Se evalúa el operador AND (&&) y sus tablas de verdad comentadas.
    - Se evalúa el operador OR (||) para condiciones alternativas.
    - Se aplica el operador NOT (!) para invertir estados y definir nuevas variables.

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,25 @@
+let mayor = false;
+let suscrito = true;
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+
+// console.log(true || true); // true
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
+
+// console.log(!true); // false
+// console.log(!false); // true
+
+// AND &&
+console.log("operador and", mayor && suscrito);
+
+// OR ||
+console.log("operador or", mayor || suscrito);
+
+//NOT !
+console.log("operador not", !mayor);
+let catalogoInfantil = !mayor;
