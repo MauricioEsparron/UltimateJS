a66798564342ad38a8cf0062a0cedc5f85f01c1a
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-12 12:44:56 -0500

    feat: experimentar con operadores de incremento y decremento en JavaScript
    - Se declaran variables numéricas básicas para pruebas aritméticas.
    - Se demuestra el post-incremento (a++) que primero evalúa y luego suma.
    - Se demuestra el pre-incremento (++a) que primero suma y luego evalúa.
    - Se detalla el uso equivalente para post-decremento (a--) y pre-decremento (--a).

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,29 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, "Adicion"); // adicion
+// console.log(a - b, "Sustraccion"); // sustraccion
+// console.log(a * b, "Multiplicacion"); // multiplicacion
+// console.log(a / b, "Division"); // division
+// console.log(a % b, "Modulo"); // modulo es lo que sobra de la division
+// console.log(a ** b, "Potencia"); // potencia
+
+// incrementar
+console.log("Valor inicial:", a);
+a++;
+console.log(a, "Despues de incrementar");
+
+console.log("-----------------------");
+console.log("Valor inicial:", a);
+++a;
+console.log(a, "Despues de incrementar");
+
+// decrementar
+console.log("Valor inicial:", a);
+a--;
+console.log(a, "Despues de decrementar");
+
+console.log("-----------------------");
+console.log("Valor inicial:", a);
+--a;
+console.log(a, "Despues de decrementar");
