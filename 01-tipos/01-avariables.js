5c82f118996a7239456ee83bf728ffca697064f0
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-12 08:47:31 -0500

    feat: 01-tipos: se agregó comentario

--- 01-tipos/01-avariables.js
+++ 01-tipos/01-avariables.js
@@ -1,4 +1,11 @@
-UpperCamelCase;
-let nombre;
-let;
+// UpperCamelCase, camelCase, snake_case;
+let nombre = "Hola mundo";
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
 console.log(nombre);
+
+let apellido, animal;
+apellido = "Ramirez";
+animal = "Perro";
+console.log(apellido, animal);
