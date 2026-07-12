0c402c4a374116e450e3c939720881785d768f1a
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-12 12:55:28 -0500

    feat: experimentar con operadores de comparación en JavaScript
    - Se declaran pruebas con operadores relacionales (>, <, >=, <=).
    - Se demuestra la diferencia entre la igualdad débil (==) y la igualdad estricta (===).
    - Se evalúan las comparaciones de desigualdad común (!=) y estricta (!==) con strings.

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,16 @@
+let a = 10;
+
+//relacionales de comparacion
+console.log(a > 5); // true
+console.log(a < 5); // false
+console.log(a >= 10); // true
+console.log(a <= 10); // true
+
+console.log("--------------");
+
+// igualdad
+console.log(a != 10); // false
+console.log(a == 10); // true
+console.log(a === 10); // true
+console.log(a === "10"); // false
+console.log(a !== "10"); // true
