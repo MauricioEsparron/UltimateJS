889b658af5b8501a350bf84aec64ab8f6da50355
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-14 14:44:11 -0500

    feat: reemplazar bucle while por ciclo for para listar números pares

---
+++ 03-control.flujo/06-for.js
@@ -0,0 +1,13 @@
+// let i = 2;
+// while (i < 2) {
+//   if (i % 2 == 0) {
+//     console.log("Número par: " + i);
+//   }
+//   i++;
+// }
+
+for (i = 0; i < 10; i++) {
+  if (i % 2 == 0) {
+    console.log("Número par: " + i);
+  }
+}
