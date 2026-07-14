1e7e4aa452a886da87a52a93c9128c04d8965fc4
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-14 14:39:18 -0500

    feat: añadir ejercicios sobre la diferencia de ejecución en bloques while y do-while

---
+++ 03-control.flujo/05-do-while.js
@@ -0,0 +1,17 @@
+//cuales son los números pares
+
+let i = 2;
+while (i < 2) {
+  if (i % 2 == 0) {
+    console.log("Número par: " + i);
+  }
+  i++;
+}
+
+// let i = 2;
+// do {
+//   if (i % 2 == 0) {
+//     console.log("Número par: " + i);
+//   }
+//   i++;
+// } while (i < 2);
