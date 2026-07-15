f13a9bd126250998d4a3111b2baceb5d59e365fb
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-14 21:32:14 -0500

    Solución del profesor con uso de operador ternario

--- 04-ejercicios/01.js
+++ 04-ejercicios/01.js
@@ -1,5 +1,6 @@
 // encontrar el numero mayor
 
+// mi solucion 1
 // function encontraMayor(a, b) {
 //   console.log("Detectemos en número mayor");
 
@@ -14,20 +15,29 @@
 // let resultado = encontraMayor(1, 5);
 // console.log("El mayor es: " + resultado);
 
-function encontraMayor(a, b) {
-  console.log("Detectemos cuál es el número mayor");
-  console.log(`los valores ingresados son: ${a} y  ${b}`);
+// mi solucion 2
+// function encontraMayor(a, b) {
+//   console.log("Detectemos cuál es el número mayor");
+//   console.log(`los valores ingresados son: ${a} y  ${b}`);
 
-  // Evaluamos cuál de los casos da como resultado 'true'
-  switch (true) {
-    case a > b:
-      return a;
-    case a < b:
-      return b;
-    default:
-      console.log("Ambos numeros son iguales");
-      return a;
-  }
+//   // Evaluamos cuál de los casos da como resultado 'true'
+//   switch (true) {
+//     case a > b:
+//       return a;
+//     case a < b:
+//       return b;
+//     default:
+//       console.log("Ambos numeros son iguales");
+//       return a;
+//   }
+// }
+// let resultado = encontraMayor(1, 5);
+// console.log(`El mayor es: ${resultado}`);
+
+// solución de Profesor
+function cualEsMayor(a, b) {
+  return a > b ? a : b;
 }
-let resultado = encontraMayor(1, 5);
-console.log(`El mayor es: ${resultado}`);
+
+let mayor = cualEsMayor(10, 5);
+console.log(mayor);
