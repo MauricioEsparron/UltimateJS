c0324204d2152bb25ce369c7544dee1c1886d7c6
MauricioEsparron <mauricio.resparron@gmail.com>
2026-07-20 21:16:57 -0500

    Solución del profesor

--- 04-ejercicios/06.js
+++ 04-ejercicios/06.js
@@ -3,16 +3,27 @@ Crear algoritmo que devuelva cantidad
 de números positivos de un array
 */
 
-let array = [2, 5, 7, 15, -5, -100, 55];
+// let array = [2, 5, 7, 15, -5, -100, 55];
+// function cuantosPositivos(arr) {
+//   let cant = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       cant++;
+//     }
+//   }
+//   return cant;
+// }
 
+//solución del profesor
+let array = [2, 5, 7, 15, -5, -100, 55];
 function cuantosPositivos(arr) {
-  let cant = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      cant++;
+  let cantidad = 0;
+  for (elemento of arr) {
+    if (elemento > 0) {
+      cantidad++;
     }
   }
-  return cant;
+  return cantidad;
 }
 
 let resultado = cuantosPositivos(array);
